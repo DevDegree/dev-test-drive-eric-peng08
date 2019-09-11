8a6279a5b59fc57aeb0419b9b82f2e0499e06564
Eric Peng <eric@Erics-MacBook-Pro-2.local>
2019-09-11 16:23:23 -0400

    added mileage return methods

--- test_drive.rb
+++ test_drive.rb
@@ -13,6 +13,17 @@ def use_headlights(brightness = "low-beam")
   puts "watch out for deer!"
 end
 
+def mileage (miles_driven, gas_used)
+if gas_used == 0
+  return 0.0
+  else 
+ return miles_driven/gas_used
+end
+end
+trip_mileage = mileage(400,12)
+lifetime_mileage = mileage(11432,366)
+puts "you got #{trip_mileage} MPG on this trip"
+puts "this car averages #{lifetime_mileage} MPG." 
 
 
 
