e9a304ffbf8dd613576355693525edf155a612b3
Eric Peng <eric@Erics-MacBook-Pro-2.local>
2019-09-11 15:59:49 -0400

    Defined the functions of the test drive car

---
+++ test_drive.rb
@@ -0,0 +1,18 @@
+def accelerate
+  puts "Stepping on the gas"
+  puts "Sppeeding up"
+end
+
+def sound_horn
+  puts "Pressing the horn button"
+  puts "BEEP BEEP!"
+end
+
+def use_headlights(brightness = "low-beam")
+  puts "turning on #{brightness} headlights"
+  puts "watch out for deer!"
+end
+
+
+
+
